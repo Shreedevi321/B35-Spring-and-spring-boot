69b8829839dd7bf5a35c703718beb93fe3d0adf3
HP <HP@DESKTOP-FCN0OTR>
2023-02-21 11:13:57 +0530

    first commit

---
+++ springwebapp-2/src/main/java/org/tnsif/sprinwebapp/Springwebapp2Application.java
@@ -0,0 +1,13 @@
+package org.tnsif.sprinwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springwebapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springwebapp2Application.class, args);
+	}
+
+}
---
+++ springwebapp-2/src/test/java/org/tnsif/sprinwebapp/Springwebapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tnsif.sprinwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springwebapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
